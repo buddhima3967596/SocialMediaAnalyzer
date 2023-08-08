cab83554e9722d5e7959b4c1c96300f019a50fe4
buddhima3967596 <113156056+buddhima3967596@users.noreply.github.com>
2023-08-08 13:08:49 +1000

    Post Class and methods, Menu class and methods

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         SocialMediaMenu menu = new SocialMediaMenu();
-        // Main method to acess menu
+        // Main method to start program
         menu.generateMenu();
 
 
